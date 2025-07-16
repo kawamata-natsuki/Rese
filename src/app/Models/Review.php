c5da98ab7e7fe69b45a54a932c198984b6050a67
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-17 01:29:24 +0900

    feat: Reviewモデル作成

--- src/app/Models/Review.php
+++ src/app/Models/Review.php
@@ -8,4 +8,33 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'shop_id',
+        'reservation_id',
+        'rating',
+        'comment',
+        'skipped_at',
+    ];
+
+    protected $casts = [
+        'skipped_at' => 'datetime',
+    ];
+
+    // リレーション定義
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
 }
