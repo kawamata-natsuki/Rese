130751300ea762671406ea9fe67d87a512f74172
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-17 01:12:19 +0900

    feat: Favoriteモデル作成

--- src/app/Models/Favorite.php
+++ src/app/Models/Favorite.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Favorite extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'shop_id',
+    ];
+
+    // リレーション定義
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
 }
