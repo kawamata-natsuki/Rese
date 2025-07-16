9ee40ddf05ed460f10d8a6d8c4da2bb8443f5e39
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-17 00:20:19 +0900

    feat: Shopモデル作成

--- src/app/Models/Shop.php
+++ src/app/Models/Shop.php
@@ -8,4 +8,38 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'area_id',
+        'genre_id',
+        'description',
+        'image_url',
+    ];
+
+    // リレーション定義
+    public function area()
+    {
+        return $this->belongsTo(Area::class);
+    }
+
+    public function genre()
+    {
+        return $this->belongsTo(Genre::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorite::class);
+    }
 }
