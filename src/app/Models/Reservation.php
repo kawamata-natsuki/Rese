052a5d5fe91f92acf6e91bb9bfae5865ccf0fa0d
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-17 00:29:26 +0900

    feat: Reservationモデル作成

--- src/app/Models/Reservation.php
+++ src/app/Models/Reservation.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Reservation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'shop_id',
+        'user_id',
+        'reservation_date',
+        'reservation_time',
+        'number_of_guests',
+        'reservation_status',
+        'visited_at',
+    ];
+
+    // リレーション定義
+    public function shop()
+    {
+        return $this->belongsTo(Shop::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
