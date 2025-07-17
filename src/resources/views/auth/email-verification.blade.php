084374d84b6d3060aa342f9e656deabbb267de8e
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-18 03:15:12 +0900

    feat: 認証系のビューファイル仮作成

---
+++ src/resources/views/auth/email-verification.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <p>メール認証誘導画面</p>
+</body>
+
+</html>
