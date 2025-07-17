606d006eab012adf6406065f284772a569ca26c6
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-18 03:29:52 +0900

    chore: 不要なコメント削除

--- src/app/Http/Controllers/User/FavoriteController.php
+++ src/app/Http/Controllers/User/FavoriteController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class FavoriteController extends Controller
 {
-    //
+    public function store()
+    {
+        //御気に入り登録処理
+    }
+
+    public function destroy()
+    {
+        // お気に入り解除処理
+    }
 }
