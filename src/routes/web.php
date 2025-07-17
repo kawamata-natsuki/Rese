678c9b6e0c6ca3afc2bd9fd094391ce72222e7af
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-17 10:08:12 +0900

    add: ログイン・ログアウト・会員登録・メール認証のルーティング設定

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,38 @@
 <?php
 
+use App\Http\Controllers\Auth\EmailVerificationController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// ===============================
+// 認証ルート
+// ===============================
 
-Route::get('/', function () {
-    return view('welcome');
+// 一般ユーザー
+Route::get('/register', [RegisterController::class, 'showRegisterView'])
+    ->name('register.view');
+Route::post('/register', [RegisterController::class, 'store'])
+    ->name('register');
+
+Route::get('/login', [LoginController::class, 'showLoginView'])
+    ->name('login.view');
+Route::post('/login', [LoginController::class, 'login'])
+    ->name('login');
+
+Route::post('/logout', [LoginController::class, 'logout'])
+    ->middleware('auth')
+    ->name('logout');
+
+Route::prefix('email')->name('verification.')->middleware('auth')->group(function () {
+    Route::get('/verify', [EmailVerificationController::class, 'notice'])
+        ->name('notice');
+    Route::get('/verify/{id}/{hash}', [EmailVerificationController::class, 'verify'])
+        ->middleware(['signed']) // URL改ざん防止
+        ->name('verify');
+    Route::post('/verification-notification', [EmailVerificationController::class, 'resend'])
+        ->middleware(['throttle:6,1']) // スパム防止
+        ->name('send');
+    Route::get('/check', [EmailVerificationController::class, 'check'])
+        ->name('check');
 });
