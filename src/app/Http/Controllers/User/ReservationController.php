26e27f8b9f953e56ed003aad14da358ca183f569
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-18 03:22:43 +0900

    chore: 不要なコメント削除

--- src/app/Http/Controllers/User/ReservationController.php
+++ src/app/Http/Controllers/User/ReservationController.php
@@ -7,5 +7,23 @@ use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function store()
+    {
+        // 予約処理
+    }
+
+    public function done()
+    {
+        return view('user.reservations.done');
+    }
+
+    public function update()
+    {
+        // 予約変更処理
+    }
+
+    public function destroy()
+    {
+        // 予約キャンセル処理
+    }
 }
