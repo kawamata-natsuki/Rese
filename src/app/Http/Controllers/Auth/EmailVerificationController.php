21c537a125f240f8bf531a365faa7efbbc6a502b
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-22 00:54:51 +0900

    feat: EmailVerificationController 作成

--- src/app/Http/Controllers/Auth/EmailVerificationController.php
+++ src/app/Http/Controllers/Auth/EmailVerificationController.php
@@ -3,27 +3,43 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 
 class EmailVerificationController extends Controller
 {
+    // メール認証誘導画面の表示
     public function notice()
     {
         return view('auth.email-verification');
     }
 
-    public function verify()
+    // メール認証の処理
+    public function verify(EmailVerificationRequest $request)
     {
-        //
+        $request->fulfill();
+        return redirect()->route('shop.index');
     }
 
-    public function resend()
+    // 認証メール再送信
+    public function resend(Request $request)
     {
-        //
+        // 認証済の場合はリダイレクト
+        if ($request->user()->hasVerifiedEmail()) {
+            return redirect()->route('shop.index');
+        }
+
+        $request->user()->sendEmailVerificationNotification();
+        return back()->with('status', 'verification-link-sent');
     }
 
+    // メール認証済みか確認してリダイレクト
     public function check()
     {
-        //
+        $user = auth()->user();
+
+        return optional($user)->hasVerifiedEmail()
+            ? redirect()->route('shop.index')
+            : redirect()->away('https://mailtrap.io/');
     }
 }
