ba65188eaa2729715837bbd1c1540df2d6e734cc
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-21 23:47:06 +0900

    feat: RegisterRequest作成

---
+++ src/app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name'      => ['required', 'max:50', 'string'],
+            'email'     => ['required', 'email', 'max:255', 'unique:users', 'string'],
+            'password'  => ['required', 'min:8', 'max:100', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => '名前を入力してください',
+            'name.max'      => '名前は50文字以内で入力してください',
+            'email.required'       => 'メールアドレスを入力してください。',
+            'email.email'          => 'メールアドレスは「ユーザー名@ドメイン」形式で入力してください',
+            'email.max'            => 'メールアドレスは255文字以内で入力してください',
+            'email.unique'         => 'このメールアドレスは既に登録されています',
+            'password.required'    => 'パスワードを入力してください',
+            'password.min'         => 'パスワードは8文字以上で入力してください',
+            'password.max'         => 'パスワードは100文字以内で入力してください',
+        ];
+    }
+
+    public function attributes(): array
+    {
+        return [
+            'name'     => '名前',
+            'email'    => 'メールアドレス',
+            'password' => 'パスワード',
+        ];
+    }
+}
