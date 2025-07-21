56fdae7d5446bdfa83bea20f90f1d6ae86dc0b42
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-07-21 21:53:12 +0900

    refactor: LoginRequestの不要なuse文を削除、ルールとメッセージの整形を改善、RememberMe機能追加

--- src/app/Http/Requests/LoginRequest.php
+++ src/app/Http/Requests/LoginRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests;
 
-use GuzzleHttp\Psr7\Message;
 use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
@@ -15,25 +14,27 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' => ['required', 'email', 'string'],
-            'password' => ['required', 'min:8', 'max:100', 'string'],
+            'email'     => ['required', 'email', 'string'],
+            'password'  => ['required', 'min:8', 'max:100', 'string'],
+            'remember'  => ['nullable', 'boolean'],
         ];
     }
 
     public function messages(): array
     {
         return [
-            'email.required' => 'メールアドレスを入力してください',
-            'email.email'    => 'メールアドレスは「ユーザー名@ドメイン」形式で入力してください',
-            'password.required' => 'パスワードを入力してください', 
-            'password.min' => 'パスワードは8文字以上で入力してください',
-            'password.max' => 'パスワードは100文字以内で入力してください',
+            'email.required'    => 'メールアドレスを入力してください',
+            'email.email'       => 'メールアドレスは「ユーザー名@ドメイン」形式で入力してください',
+            'password.required' => 'パスワードを入力してください',
+            'password.min'      => 'パスワードは8文字以上で入力してください',
+            'password.max'      => 'パスワードは100文字以内で入力してください',
         ];
     }
 
     public function attributes(): array
     {
-        return[
+        // デフォルトのエラーメッセージ内で使われるカラム名を変換
+        return [
             'email'      => 'メールアドレス',
             'password'   => 'パスワード',
         ];
