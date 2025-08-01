56556bb25853e95e434b964f0309c0d1375d1684
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-01 16:08:14 +0900

    feat: 店舗一覧にエリアとジャンルの情報を追加

--- src/app/Http/Controllers/Shop/ShopController.php
+++ src/app/Http/Controllers/Shop/ShopController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers\Shop;
 
 use App\Http\Controllers\Controller;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
     public function index()
     {
-        return view('shop.index');
+        $shops = Shop::with(['area', 'genre'])->get();
+        return view('shop.index', compact('shops'));
     }
 
     public function show($shop)
