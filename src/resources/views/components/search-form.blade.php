3992cd8367897553a3f38e3ac9acd9fa3b4d19a9
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-01 23:42:03 +0900

    refactor: <form>の method と action を削除、セレクトボックスのoptionからrequestを除去

--- src/resources/views/components/search-form.blade.php
+++ src/resources/views/components/search-form.blade.php
@@ -1,15 +1,15 @@
 <!-- 検索フォーム -->
 <div class="header__search">
 
-  <form id="search-form" class="search-form" method="GET" action="{{ route('shop.search') }}">
+  <form id="search-form" class="search-form">
 
     <div class="search-form__group   search-form__group--select">
       <div class="search-form__select-wrapper">
         <select name="area" id="area" class="search-form__select">
-          <option value="all" {{ request('area', 'all') == 'all' ? 'selected' : '' }}>すべてのエリア</option>
-          <option value="tokyo" {{ request('area') == 'tokyo' ? 'selected' : '' }}>東京都</option>
-          <option value="osaka" {{ request('area') == 'osaka' ? 'selected' : '' }}>大阪府</option>
-          <option value="fukuoka" {{ request('area') == 'fukuoka' ? 'selected' : '' }}>福岡県</option>
+          <option value="all">すべてのエリア</option>
+          <option value="1">東京都</option>
+          <option value="2">大阪府</option>
+          <option value="3">福岡県</option>
         </select>
         <span class="search-form__select-icon">
           <i class="fas fa-chevron-down"></i>
@@ -21,11 +21,11 @@
       <div class="search-form__select-wrapper">
         <select name="genre" id="genre" class="search-form__select">
           <option value="all">すべてのジャンル</option>
-          <option value="sushi" {{ request('genre') == 'sushi' ? 'selected' : '' }}>寿司</option>
-          <option value="yakiniku" {{ request('genre') == 'yakiniku' ? 'selected' : '' }}>焼肉</option>
-          <option value="izakaya" {{ request('genre') == 'izakaya' ? 'selected' : '' }}>居酒屋</option>
-          <option value="italian" {{ request('genre') == 'italian' ? 'selected' : '' }}>イタリアン</option>
-          <option value="ramen" {{ request('genre') == 'ramen' ? 'selected' : '' }}>ラーメン</option>
+          <option value="1">寿司</option>
+          <option value="2">焼肉</option>
+          <option value="3">居酒屋</option>
+          <option value="4">イタリアン</option>
+          <option value="5">ラーメン</option>
         </select>
         <span class="search-form__select-icon">
           <i class="fas fa-chevron-down"></i>
@@ -49,15 +49,4 @@
     </div>
 
   </form>
-
-  {{--
-  <!-- 検索結果表示エリア -->
-  <div id="search-results" class="search-results">
-    <!-- JavaScriptで動的に結果を追加します    -->
-    @foreach ($shops as $shop)
-    @include('components.shop-card', ['shop' => $shop])
-    @endforeach 
-  --}}
-</div>
-
 </div>
