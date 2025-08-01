c1f825a26820ab2574e252efdbc70a6e7867e694
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-01 23:47:31 +0900

    feat: 店舗詳細ページの blade ファイルと css ファイル作成

--- src/resources/views/shop/show.blade.php
+++ src/resources/views/shop/show.blade.php
@@ -1,14 +1,61 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/shop/show.css') }}">
+@endsection
 
-<body>
-  <p>飲食店詳細ページ</p>
-</body>
+@section('title', '飲食店詳細')
 
-</html>
+@section('content')
+<div class="shop-show-page">
+  <div class="shop-show-page__container">
+
+    <!-- 左側：店舗情報 -->
+    <div class="shop-show-page__info-wrapper">
+
+      <!-- 店舗名 -->
+      <p class="shop-show-page__name">{{ $shop->name }}</p>
+
+      <!-- 店舗画像 -->
+      <div class="shop-show-page__image-wrapper">
+        <img src="{{ $shop->image_url }}" alt="{{ $shop->name }}の画像" class="shop-show-page__image">
+      </div>
+
+      <!-- エリア＋ジャンル -->
+      <div class="shop-show-page__tags">
+        <span class="shop-show-page__tag is-area">
+          # {{ $shop->area->name }}
+        </span>
+        <span class="shop-show-page__tag is-genre">
+          # {{ $shop->genre->name }}
+        </span>
+      </div>
+
+      <!-- 店舗紹介 -->
+      <p class="shop-show-page__description">
+        {{ $shop->description }}
+      </p>
+    </div>
+
+    <!-- 右側：予約フォーマット -->
+    <div class="reservation-section">
+      <form action="{{ route('user.reservations.store') }}" method="post" class="reservation-form">
+        <h1 class="reservation-form__heading">予約</h1>
+
+        <!-- 日にち選択 -->
+        <!-- 時間選択 -->
+        <!-- 人数選択 -->
+
+        <!-- フォーム入力結果表示 -->
+
+        <!-- 予約するボタン -->
+      </form>
+    </div>
+
+  </div>
+
+</div>
+
+
+
+@endsection
