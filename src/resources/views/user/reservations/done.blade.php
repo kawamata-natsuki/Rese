62d44f4b0694ec7b17be145183996480bd52fbdc
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-04 01:16:25 +0900

    feat: 予約完了ページをbladeに変更

--- src/resources/views/user/reservations/done.blade.php
+++ src/resources/views/user/reservations/done.blade.php
@@ -1,14 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
+@section('css')
+@endsection
 
-<body>
-  <p>予約完了ページ</p>
-</body>
+@section('title', '予約完了')
 
-</html>
+@section('content')
+<h1>予約完了</h1>
+@endsection
