3b96be0ec7411afe8f8b3e7db7d9180de1082b86
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-04 01:17:42 +0900

    feat: 予約処理実装

--- src/app/Http/Controllers/User/ReservationController.php
+++ src/app/Http/Controllers/User/ReservationController.php
@@ -3,13 +3,41 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ReservationRequest;
+use App\Models\Reservation;
+use App\Models\Shop;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    public function store()
+    // 予約処理
+    public function store(ReservationRequest $request)
     {
-        // 予約処理
+        $shop = Shop::findOrFail($request->shop_id);
+
+        // スロットを動的に生成
+        $opening = $shop->opening_time->copy();
+        $closing = $shop->closing_time;
+
+        $timeSlots = [];
+        while ($opening < $closing) {
+            $timeSlots[] = $opening->format('H:i');
+            $opening->addMinutes(30);
+        }
+
+        $numberSlots = range(1, 10);
+
+        $validated = $request->validated();
+        Reservation::create([
+            'user_id'  => auth()->id(),
+            'shop_id'  => $validated['shop_id'],
+            'reservation_date' => $validated['date'],
+            'reservation_time' => $validated['time'],
+            'number_of_guests' => $validated['number'],
+        ]);
+
+        return redirect()
+            ->route('user.reservations.done');
     }
 
     public function done()
