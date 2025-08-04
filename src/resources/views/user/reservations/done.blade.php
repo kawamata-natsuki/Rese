1220ce10fa2d27c2310a235c62463e95117ee761
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-04 13:16:44 +0900

    feat: 予約完了画面にマイページ・トップボタン追加、レイアウト調整

--- src/resources/views/user/reservations/done.blade.php
+++ src/resources/views/user/reservations/done.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.app')
 
 @section('css')
+<link rel="stylesheet" href="{{ asset('css/user/reservations/done.css') }}">
 @endsection
 
 @section('title', '予約完了')
 
 @section('content')
-<h1>予約完了</h1>
+<div class="done-page">
+  <div class="done-page__card">
+    <h1 class="done-page__title">ご予約が完了しました</h1>
+    <p class="done-page__message">ご予約ありがとうございます。<br>予約内容はマイページから確認できます。</p>
+
+    <div class="done-page__button-wrapper">
+      <a href="{{ route('user.mypage.index') }}" class="done-page__button">マイページへ</a>
+      <a href="{{ route('shop.index') }}" class="done-page__button done-page__button--secondary">トップに戻る</a>
+    </div>
+  </div>
+</div>
 @endsection
