39b9639986ec879a1aa1d1649d6b5b183cad3afc
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-04 16:14:59 +0900

    feat: ユーザー情報取得の処理追加

--- src/app/Http/Controllers/User/MypageController.php
+++ src/app/Http/Controllers/User/MypageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Models\Shop;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -9,6 +10,16 @@ class MypageController extends Controller
 {
     public function index()
     {
-        return view('user.mypage.index');
+        $user = auth()->user();
+
+        // ユーザーの予約とお気に入りを取得
+        $reservations = $user->reservations()->with('shop.area', 'shop.genre')->get();
+        $favoriteShops = $user->favoriteShops;
+
+        return view('user.mypage.index', [
+            'user' => $user,
+            'reservations'  => $reservations,
+            'favoriteShops' => $favoriteShops,
+        ]);
     }
 }
