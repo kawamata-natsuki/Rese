9e2ef33393d4752b7c60aed421357b0e5fd3ef0e
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-05 22:48:26 +0900

    remove: softDeltes カラムを削除

--- src/database/migrations/2025_07_16_011342_create_areas_table.php
+++ src/database/migrations/2025_07_16_011342_create_areas_table.php
@@ -11,7 +11,6 @@ return new class extends Migration
         Schema::create('areas', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->softDeletes();
             $table->timestamps();
         });
     }
