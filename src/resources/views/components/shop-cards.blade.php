26d90be841f6c7ca55a480aba15803af2441c1a6
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-21 10:18:53 +0900

    feat: 店舗一覧ページにページネーション追加

--- src/resources/views/components/shop-cards.blade.php
+++ src/resources/views/components/shop-cards.blade.php
@@ -10,4 +10,12 @@
   <x-shop-card :shop="$shop" />
   @endforeach
 </div>
+
+{{-- ページネーション --}}
+@if ($shops->hasPages())
+<div class="pagination-wrapper">
+  {{-- 端のページのリンク数を少なめにするなら onEachSide(1) などもOK --}}
+  {{ $shops->onEachSide(1)->links('vendor.pagination.custom') }}
+</div>
+@endif
 @endif
