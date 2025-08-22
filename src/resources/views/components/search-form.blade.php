372b11f40fc5e788d757e58cba035843b5f601eb
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-08-22 09:25:22 +0900

    style: 検索フォームのコード整形とインデントの修正

--- src/resources/views/components/search-form.blade.php
+++ src/resources/views/components/search-form.blade.php
@@ -1,10 +1,8 @@
 <!-- 検索フォーム -->
 <div class="header__search">
-
   <form id="search-form" class="search-form">
-
     <!-- エリア検索 -->
-    <div class="search-form__group   search-form__group--select">
+    <div class="search-form__group search-form__group--select">
       <div class="search-form__select-wrapper">
         <select name="area" id="area" class="search-form__select">
           <option value="all">すべてのエリア</option>
@@ -12,9 +10,7 @@
           <option value="2">大阪府</option>
           <option value="3">福岡県</option>
         </select>
-        <span class="search-form__select-icon">
-          <i class="fas fa-chevron-down"></i>
-        </span>
+        <span class="search-form__select-icon"><i class="fas fa-chevron-down"></i></span>
       </div>
     </div>
 
@@ -29,34 +25,23 @@
           <option value="4">イタリアン</option>
           <option value="5">ラーメン</option>
         </select>
-        <span class="search-form__select-icon">
-          <i class="fas fa-chevron-down"></i>
-        </span>
+        <span class="search-form__select-icon"><i class="fas fa-chevron-down"></i></span>
       </div>
     </div>
 
     <!-- キーワード検索 -->
     <div class="search-form__group search-form__group--input">
       <div class="search-form__input-wrapper">
-        <span class="search-form__search-icon">
-          <i class="fas fa-search"></i>
-        </span>
-        <input
-          type="text"
-          name="keyword"
-          id="keyword"
+        <span class="search-form__search-icon"><i class="fas fa-search"></i></span>
+        <input type="text" name="keyword" id="keyword"
           value="{{ request('keyword') }}"
-          placeholder="Search..."
-          class="search-form__input">
+          placeholder="Search..." class="search-form__input">
       </div>
     </div>
 
     <!-- リセットボタン -->
     <div class="search-form__group search-form__group--reset">
-      <button type="button" id="reset-button" class="search-form__reset-button">
-        リセット
-      </button>
+      <button type="button" id="reset-button" class="search-form__reset-button">リセット</button>
     </div>
-
   </form>
 </div>
