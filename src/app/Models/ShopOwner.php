504f943fde3c51108c12e7776ef5478cdd8f4744
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-09-10 15:09:26 +0900

    feat: ShopOwnerモデルを作成し、fillableプロパティとリレーションを定義

---
+++ src/app/Models/ShopOwner.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+
+class ShopOwner extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    // リレーション
+    public function shops()
+    {
+        return $this->hasMany(Shop::class);
+    }
+}
