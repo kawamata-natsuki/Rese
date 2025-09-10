64f5af718c4f42d069c5afc55fa901c100a1704d
kawamata-natsuki <t.natsuki020817@gmail.com>
2025-09-10 10:51:18 +0900

    refactor: AdminモデルをAuthenticatableに変更し、Notifiableトレイトを追加

--- src/app/Models/Admin.php
+++ src/app/Models/Admin.php
@@ -3,17 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
-    protected $fillable = ['email', 'password'];
-    protected $hidden   = ['password', 'remember_token'];
+    protected $guard = 'admin';
+
+    protected $fillable = [
+        'email',
+        'password'
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token'
+    ];
 
     protected function casts(): array
     {
-        return ['password' => 'hashed'];
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
     }
 }
